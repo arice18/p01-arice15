7a03fdb9504601d1074af9a9188199196bd75c59
arice18 <arice15@ecc-jaguar.csuchico.edu>
2017-09-26 03:49:38 -0700

    It seems that the random number changes and the loop starts over.

--- guess.cpp
+++ guess.cpp
@@ -46,13 +46,68 @@ int main () {
                 int guess2;
 
                 srand ( time(NULL) );
-                int randomNumber;
-                randomNumber = rand () % 10 + 1;
+                int randomNumber2;
+                randomNumber2 = rand () % 10 + 1;
                 
                 cout << "You have 2 guesses. Pick a number between 1 and 10: ";
                 cin >> guess2;
                 
-                if ( guess2 
+                if ( guess2 < randomNumber2 ) {
+                    int guess3;
+
+                    cout << "The number is higher, try again." << endl;
+                    cin >> guess3;
+
+                    if (guess3 = randomNumber2 ) {
+                        int proceedA;
+                        cout << "You win! Press any key to play again or press 1 to quit: " << endl;
+                        cin >> proceedA;
+                        if (proceedA == 1 ) {
+                            return 0;
+                        }
+                    else {
+                        int proceedA;
+                        cout << "You lose! Press any key to play again or press 1 to quit: " << endl;
+                        cin >>  proceedA;
+                        if ( proceedA == 1) {
+                            return 0;
+                        }
+                    }
+                    }
+                }
+
+                if ( guess2 > randomNumber2 ) {
+                        int guess4;
+
+                        cout << "The number is lower, try again." << endl;
+                        cin >> guess4;
+
+                        if (guess4 == randomNumber2 ) {
+                            int proceedB;
+                            cout << "You win! Press any key to play again or press 1 to quit: " << endl;
+                            cin >> proceedB;
+                            if (proceedB == 1 ) {
+                                return 0;
+                            }
+                        else {
+                            int proceedB;
+                            cout << "You lose! Press any key to play again or press 1 to quit: " << endl;
+                            cin >> proceedB;
+                            if (proceedB == 1) {
+                                return 0;
+                            }
+                        }
+                        }
+                }
+                if ( guess2 == randomNumber2 ) {
+                    int proceedC;
+                    cout << "You win! Press any key to play again or press 1 to quit: " <<endl;
+                    cin >> proceedC;
+                    if (proceedC == 1 ) {
+                        return 0;
+                    }
+                }
+
                 break;
             case 3: 
                 difficulty = true;
